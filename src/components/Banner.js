76326e6c5ca4830244ad10cd1c3405fe922d225e
SanketLakhani <sanketlakhani80@gmail.com>
2023-09-01 23:11:46 +0530

    made some changes in plans screen

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -19,7 +19,7 @@ function Banner() {
     fetchData();
   }, []);
 
-  console.log(movie);
+  // console.log(movie);
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
--- src/components/PlansScreen.js
+++ src/components/PlansScreen.js
@@ -8,6 +8,24 @@ import { loadStripe } from "@stripe/stripe-js";
 function PlansScreen() {
   const [products, setProducts] = useState([]);
   const user = useSelector(selectUser);
+  const [subscription, setSubcription] = useState(null);
+
+  useEffect(() => {
+    db.collection("customers")
+      .doc(user.uid)
+      .collection("subscriptions")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async (subscription) => {
+          setSubcription({
+            role: subscription.data().role,
+            current_period_end: subscription.data().current_period_end.seconds,
+            current_period_start:
+              subscription.data().current_period_start.seconds,
+          });
+        });
+      });
+  }, [user.uid]);
 
   useEffect(() => {
     db.collection("products")
@@ -30,6 +48,7 @@ function PlansScreen() {
   }, []);
 
   console.log(products);
+  console.log(subscription);
 
   const loadCheckout = async (priceId) => {
     const docRef = await db
@@ -37,15 +56,13 @@ function PlansScreen() {
       .doc(user.uid)
       .collection("checkout_sessions")
       .add({
-        price: "price_1NidAHSJQYvVkVusJQ4FtpQI",
+        price: priceId,
         success_url: window.location.origin,
         cancel_url: window.location.origin,
       });
 
     docRef.onSnapshot(async (snap) => {
       const { error, sessionId } = snap.data();
-      // console.log(sessionId);
-      // console.log(error);
 
       if (error) {
         // Show an error to your customer and
@@ -66,16 +83,36 @@ function PlansScreen() {
 
   return (
     <div className="plansScreen">
+      <br />
+      {subscription && (
+        <p>
+          Renewal date:{" "}
+          {new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString()}
+        </p>
+      )}
       {Object.entries(products).map(([productId, productData]) => {
+        const isCurrentPackage = productData.name?.includes(subscription?.role);
+
         return (
-          <div className="plansScreen__plan" key={productId}>
+          <div
+            className={`${
+              isCurrentPackage && "plansScreen__plan--disabled"
+            } plansScreen__plan`}
+            key={productId}
+          >
             <div className="plansScreen__info">
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
             </div>
 
-            <button onClick={() => loadCheckout(productData.prices.priceId)}>
-              Subscribe
+            <button
+              onClick={() =>
+                isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Package" : "Subscribe"}
             </button>
           </div>
         );
